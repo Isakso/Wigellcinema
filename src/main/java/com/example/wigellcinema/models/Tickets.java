1de9cd7e5edd5a262ee156cc32ad707e21da1e71
Isaac <asabaisaac1@gmail.com>
2024-06-15 15:38:04 +0200

    updated ticket

--- src/main/java/com/example/wigellcinema/models/Tickets.java
+++ src/main/java/com/example/wigellcinema/models/Tickets.java
@@ -1,4 +1,21 @@
 package com.example.wigellcinema.models;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Tickets {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ticket_id")
+    private int id;
+
+    @Column(name = "ticket_type")
+    private String ticketType;
+
+
+
+
+
+
+
 }
