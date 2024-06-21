71323f36ec3c5c124b1844b8f96f9282b80e908e
Isaac <asabaisaac1@gmail.com>
2024-06-21 23:35:16 +0200

    latest pull

--- src/main/java/com/example/wigellcinema/WigellCinemaApplication.java
+++ src/main/java/com/example/wigellcinema/WigellCinemaApplication.java
@@ -2,12 +2,13 @@ package com.example.wigellcinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-//EnableEurekaClient
+@EnableDiscoveryClient
 //@EntityScan("com.example.wigellcinema.models")
 
 
